c561d2ac4e9e8dc36597e63d8903d5559178741c
schoolmanage <wb.web03@gmail.com>
2026-06-17 21:34:54 +0530

    database updated

--- madrasah/settings.py
+++ madrasah/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,12 +79,21 @@ WSGI_APPLICATION = 'madrasah.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+if DEBUG :
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+else:
+    DATABASES = {
+        'default' :{
+            dj_database_url.config(
+                default =os.getenv("DATABASE_URL")
+            )
+        }
+    }
 
 
 # Password validation
